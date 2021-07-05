43ccfc65b1a184115615abc0bac937fc8eda933b
collatedefi <86946661+collatedefi@users.noreply.github.com>
2021-07-05 18:15:10 +0800

    v0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import GoogleData from './Google';
+import { RiCopyrightFill } from 'react-icons/ri'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <RiCopyrightFill className="App-logo" />
+        <h1>
+          Collate
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read More
         </a>
+        <GoogleData />
       </header>
     </div>
   );
